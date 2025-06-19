5d84f5a370a2068724bea10ef28d9aee08d027e4
allan <allan.rolle@laposte.net>
2025-06-19 17:17:19 +0200

    weather app using api complete

--- weather-app/src/app/app.spec.ts
+++ weather-app/src/app/app.spec.ts
@@ -1,23 +1,21 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('App', () => {
+  let app: App;
+  let fixture: ComponentFixture<App>;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
+    fixture = TestBed.createComponent(App);
+    app = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, weather-app');
   });
 });
--- weather-app/src/views/weather-card/weather-card.spec.ts
+++ weather-app/src/views/weather-card/weather-card.spec.ts
@@ -1,23 +1,79 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { WeatherCard } from './weather-card';
+import { By } from '@angular/platform-browser';
 
 describe('WeatherCard', () => {
   let component: WeatherCard;
   let fixture: ComponentFixture<WeatherCard>;
+  const mockData = [
+    {
+      datetime: '2025-06-19',
+      datetimeEpoch: 1750284000,
+      tempmax: 31.0,
+      tempmin: 18.0,
+      temp: 25.1,
+      feelslikemax: 30.0,
+      feelslikemin: 18.0,
+      feelslike: 24.7,
+      dew: 13.6,
+      humidity: 51.4,
+      precip: 0.0,
+      precipprob: 0.0,
+      precipcover: 0.0,
+      preciptype: null,
+      snow: 0.0,
+      snowdepth: 0.0,
+      windgust: 33.1,
+      windspeed: 16.6,
+      winddir: 45.1,
+      pressure: 1022.9,
+      cloudcover: 4.2,
+      visibility: 18.7,
+      solarradiation: 308.3,
+      solarenergy: 26.7,
+      uvindex: 9.0,
+      severerisk: 10.0,
+      sunrise: '05:46:57',
+      sunriseEpoch: 1750304817,
+      sunset: '21:57:00',
+      sunsetEpoch: 1750363020,
+      moonphase: 0.79,
+      conditions: 'Clear',
+      description: 'Clear conditions throughout the day.',
+      icon: 'clear-day',
+      stations: [
+        '91027002',
+        '75116008',
+        '78620001',
+        '78640001',
+        '75114001',
+        '95088001',
+        'C1292',
+        'LFPO',
+        'remote',
+      ],
+      source: 'comb',
+    },
+  ];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [WeatherCard]
-    })
-    .compileComponents();
+      imports: [WeatherCard],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(WeatherCard);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput('dataFromApi', mockData);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render the data correctly', () => {
+    const paragraphs = fixture.debugElement.queryAll(By.css('p'));
+    expect(paragraphs[0].nativeElement.textContent.split(' ').length).toBe(3);
+    expect(paragraphs.length).toBe(3);
   });
 });
