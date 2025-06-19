0241bc41486860c24cc11f1f4de9dfe1e6405425
allan <allan.rolle@laposte.net>
2025-06-19 11:25:13 +0200

    create weather card component

--- weather-app/src/app/app.ts
+++ weather-app/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WeatherCard } from '../views/weather-card/weather-card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, WeatherCard],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected title = 'weather-app';
