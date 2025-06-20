80e92e71bb9be054e48af7fa62e275b79e0c36ef
allan <allan.rolle@laposte.net>
2025-06-20 14:31:24 +0200

    header complete

---
+++ todo-list/src/app/todos/add-todo-form/add-todo-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-todo-form',
+  imports: [],
+  templateUrl: './add-todo-form.html',
+  styleUrl: './add-todo-form.scss'
+})
+export class AddTodoForm {
+
+}
