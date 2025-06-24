197908a6665f07563b854690fbbd7e4ce8eb30a5
allan <allan.rolle@laposte.net>
2025-06-24 13:26:31 +0200

    Header complete

---
+++ photo-gallery/src/app/features/gallery/gallery-filters-panel/gallery-filters-panel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery-filters-panel',
+  imports: [],
+  templateUrl: './gallery-filters-panel.component.html',
+  styleUrl: './gallery-filters-panel.component.scss'
+})
+export class GalleryFiltersPanelComponent {
+
+}
---
+++ photo-gallery/src/app/features/gallery/gallery-images/gallery-images.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery-images',
+  imports: [],
+  templateUrl: './gallery-images.component.html',
+  styleUrl: './gallery-images.component.scss'
+})
+export class GalleryImagesComponent {
+
+}
